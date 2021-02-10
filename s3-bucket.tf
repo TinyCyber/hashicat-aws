3ab92a8af1b49ff301ffcf4ccb1c668909b10dbe
Your Name <tiny.mpetersii@gmail.com>
2021-02-10 19:48:16 +0000

    added s3 bucket module again

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,7 +1,10 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-  bucket prefix = "Mark Peters"
+  
+  bucket = "Mark-Peters"
   acl    = "private"
+  create_bucket = false
+  force_destroy = true
 
   versioning = {
     enabled = true
